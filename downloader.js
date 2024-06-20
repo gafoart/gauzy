d864576d23fa77352cfbc35ae8bc697db6a72040
gafoart <150106861+gafoart@users.noreply.github.com>
2024-06-19 23:13:17 -0400

    Update downloader.js

--- downloader.js
+++ downloader.js
@@ -1,8 +1,5 @@
 async function download_splat(url_param) {
-    const url = new URL(
-        url_param,
-        "https://huggingface.co/datasets/satyoshi/gauzilla-data/resolve/main/",
-    );
+    const url = new URL(url_param);
 
     const req = await fetch(url, {
         mode: "cors",
@@ -22,13 +19,6 @@ async function download_splat(url_param) {
         splatData.set(value, bytesRead);
         bytesRead += value.length;
 
-        /*
-        FIXME: fails with large splat files:
-
-        Uncaught (in promise) DOMException:
-        Failed to execute 'postMessage' on 'DedicatedWorkerGlobalScope':
-        Data cannot be cloned, out of memory
-        */
         postMessage({
             bytes: bytesRead,
             buffer: splatData,
@@ -36,46 +26,7 @@ async function download_splat(url_param) {
     }
 }
 
-
-/*
-async function download_splat2() {
-    const url = new URL(
-        "train.splat",
-        "https://huggingface.co/cakewalk/splat-data/resolve/main/",
-    );
-
-    const req = await fetch(url, {
-        mode: "cors",
-        credentials: "omit",
-    });
-    console.log(req);
-    if (req.status != 200)
-        throw new Error("download_splat(): HTTP status: " + req.status + ", failed to load " + req.url);
-
-    const reader = req.body.getReader();
-    const cl = parseInt(req.headers.get("content-length"));
-    let bytesRead = 0;
-    while (true) {
-        const { done, value } = await reader.read();
-        if (done) break;
-
-        bytesRead += value.length;
-
-        postMessage({
-            conlen: cl,
-            bytes: bytesRead,
-            chunk: value,
-        });
-    }
-}
-
-(async () => {
-    await download_splat2();
-})();
-*/
-
-
 self.onmessage = async function(event) {
-    console.log("downloader.js: Received message from Rust:", event.data);
-    download_splat(event.data);
+    console.log("downloader.js: Received message:", event.data);
+    await download_splat(event.data);
 };
